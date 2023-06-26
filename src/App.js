7feec839c7712e8b0dedb15bd2068d8913579d19
Adriviciano <97399669+Adriviciano@users.noreply.github.com>
2023-06-26 12:33:52 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header/Header.js';
+import Movies from './components/movies/Movies.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Movies/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/movies/Movies.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './Movies.css'
+
+export default function Movies(){
+  return(
+    <div className="moviesContainer">
+      <div>
+        MIRAR COMO FUNCIONA LA API DE MOVIEDB
+      </div>
+      <br/><br/>
+      <div>
+        Clave de la api:
+        <div>
+          5abe8300a9b1cf8dda9b458ba1420a0e
+        </div>
+      </div>
+      <br/><br/>
+      <div>
+        Token de acceso:
+        <div>
+          eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWJlODM
+          wMGE5YjFjZjhkZGE5YjQ1OGJhMTQyMGEwZSIsInN
+          1YiI6IjY0OGI3ZGRkNTU5ZDIyMDBhZDgwZDVjYiI
+          sInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9
+          uIjoxfQ.G_7P2OmjRVIWwZBgZClL7RnhwI0Qpo6p
+          HeGq0B5-cpo
+        </div>
+      </div>
+    </div>
+  )
+}
